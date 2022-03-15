9dccc0313c74f07d7aee3850e0fc12b1dbb61c88
Rajat Patil <patilrajat277@gmail.com>
2022-03-15 19:50:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,20 +9,5 @@ function BackgroundSelector(){
 	body.style.background = "linear-gradient(to right, " + color1.value + "," + color2.value + ")";
 	css.textContent = body.style.background + ";";
 }
-
-// color1.addEventListener("input",function(){
-// 	// console.log(color1.value);
-// 	// This will assign only single color to background
-// 	// we have to use linear-gradient css property here
-// 	// body.style.background = color1.value;
-// 	body.style.background = "linear-gradient(to right, " + color1.value + "," + color2.value + ")";
-// });
-
-// color2.addEventListener("input",function(){
-// 	// console.log(color2.value);
-// 	// body.style.background = color2.value;
-// 	body.style.background = "linear-gradient(to right, " + color1.value + "," + color2.value + ")";
-// });
-
 color1.addEventListener("input",BackgroundSelector);
 color2.addEventListener("input",BackgroundSelector);
